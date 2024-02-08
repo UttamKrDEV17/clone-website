ba51ca273701d1cd11f978f83b833c04a8d207b8
UTTAM KUMAR <133669263+UttamKrDEV17@users.noreply.github.com>
2024-02-08 22:09:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+const locoscroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true,
+});
+
+function videoanimation(){
+    var videocon = document.querySelector('#video-container');
+    var playbtn = document.querySelector('#play');
+
+
+videocon.addEventListener("mouseenter",()=>{
+    gsap.to(playbtn,{
+        scale: 1,
+        opacity: 1,
+    })
+});
+
+videocon.addEventListener("mouseleave",()=>{
+    gsap.to(playbtn,{
+        scale: 0,
+        opacity: 0,
+    })
+});
+
+videocon.addEventListener("mousemove",(dets)=>{
+    gsap.to(playbtn,{
+        left:dets.x,
+        top:dets.y,
+    })
+});
+};
+function loadinganimation(){
+    gsap.from("#page1 h1",{
+        y:100,
+        opacity: 0,
+        delay:0.5,
+        duration:0.5,
+        stagger: 0.4,
+    })
+    gsap.from("#page1 #video-container",{
+        scale:0.9,
+        opacity: 0,
+        delay:1.2,
+        duration:0.3,
+    })
+}
+
+
+videoanimation();
+loadinganimation();
+
